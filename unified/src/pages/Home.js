5c36e05f2d2c10e72d8e598f1ff4cbfb8831db10
cagla istanbulluoglu <cagla.istanbulluoglu@gmail.com>
2022-07-06 16:40:09 -0700

    first commit

---
+++ unified/src/pages/Home.js
@@ -0,0 +1,22 @@
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  // page content
+  
+  return (
+    <Navbar bg="light" variant="light">
+    <Container>
+    <Navbar.Brand as={Link} to="/">UniFied</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link as={Link} to="/">Home</Nav.Link>
+      <Nav.Link as={Link} to="/about">About</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+  )
+}
+
+export default Home
