b024e6695932367368de54e6e6a189771ca54151
catnip1032 <catherine_riopel@hotmail.com>
2022-07-15 21:35:02 -0700

    Add Events page to routes

--- unified/src/App.tsx
+++ unified/src/App.tsx
@@ -5,6 +5,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
+import Events from "./pages/Events";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/about" element={<About />} />
+        <Route path="/events" element={<Events />} />
         <Route element={<NotFound />} />
       </Routes>
     </Container>
