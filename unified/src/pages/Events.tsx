2af770b34688af81950c82040e5d97b0a0675360
catnip1032 <catherine_riopel@hotmail.com>
2022-07-16 13:58:07 -0700

    Add event scaffold to the events page

--- unified/src/pages/Events.tsx
+++ unified/src/pages/Events.tsx
@@ -1,10 +1,14 @@
+import EventScaffold from '../components/Events/EventScaffold';
 import NavigationBar from '../components/NavigationBar';
 
 const Events = () => {
   // page content
   
   return (
-    <NavigationBar/>
+    <>
+        <NavigationBar/>
+        <EventScaffold></EventScaffold>
+    </>
   )
 }
 
