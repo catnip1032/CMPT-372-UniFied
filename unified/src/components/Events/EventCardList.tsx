f5834f9b7aa932b6555921adcb8f3efd580fc6ac
catnip1032 <catherine_riopel@hotmail.com>
2022-07-15 21:36:36 -0700

    Create components related to the Events page

---
+++ unified/src/components/Events/EventCardList.tsx
@@ -0,0 +1,10 @@
+// A list that holds event cards
+import React from "react"
+
+const EventCardList = () => {
+    return (
+        <></>
+    )
+}
+
+export default EventCardList
---
+++ unified/src/components/Events/EventSortingBar.tsx
@@ -0,0 +1,10 @@
+// Bar for sorting components
+import React from "react"
+
+const EventSortingBar = () => {
+    return (
+        <></>
+    )
+}
+
+export default EventSortingBar
