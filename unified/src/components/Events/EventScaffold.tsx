dccdd7379a4ad4c1107f8f2ec69b451c01fa57e4
catnip1032 <catherine_riopel@hotmail.com>
2022-07-16 13:57:42 -0700

    Create and pass functions to handle sorting bar state changes

--- unified/src/components/Events/EventScaffold.tsx
+++ unified/src/components/Events/EventScaffold.tsx
@@ -1,14 +1,45 @@
-import React from "react"
-
 import EventSortingBar from "./EventSortingBar"
 import EventCardList from "./EventCardList"
+import { useState } from "react"
+
 
 const EventScaffold = () => {
+    const [daySorting , setDaySorting] = useState("Whenever")
+    const [typeSorting , setTypeSorting] = useState("Any")
+    const [distanceSorting , setDistanceSorting] = useState("Whereever")
+    const [categorySorting , setCategorySorting] = useState("Any")
+    const [universitySorting , setUniversitySorting] = useState("Any")
+
+    const sortingBarOnClicks = [
+        (sortType: any) => { 
+            setDaySorting(sortType); 
+        },
+        (sortType: any) => {
+            setTypeSorting(sortType); 
+        },
+        (sortType: any) => {
+            setDistanceSorting(sortType);
+        },
+        (sortType: any) => {
+            setCategorySorting(sortType);
+        },
+        (sortType: any) => {
+            setUniversitySorting(sortType); 
+        },
+    ]
+
+    const eventCardListSortingProps = [
+        daySorting,
+        typeSorting,
+        distanceSorting,
+        categorySorting,
+        universitySorting
+    ]
 
     return (
         <>
-            <EventSortingBar></EventSortingBar>
-            <EventCardList></EventCardList>
+            <EventSortingBar sortingOnClicks={sortingBarOnClicks}></EventSortingBar>
+            <EventCardList sortingCombinations={eventCardListSortingProps}></EventCardList>
         </>
     )
 
