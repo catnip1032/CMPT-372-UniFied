6123e894bf1f69492b6259705e7eefa7374c58b2
catnip1032 <catherine_riopel@hotmail.com>
2022-07-16 18:53:37 -0700

    Create event card to include event image, title, date, and location

--- unified/src/components/Events/EventCard.tsx
+++ unified/src/components/Events/EventCard.tsx
@@ -1,9 +1,33 @@
 // A card that holds events
-import React from "react"
+import Card from 'react-bootstrap/Card';
 
-const EventCard = () => {
+interface EventCardProps {
+    date: string,
+    title: string,
+    location: string,
+    image: string,
+}
+
+
+const EventCard = (eventCardProps: EventCardProps) => {
+    let date = eventCardProps.date;
+    let title = eventCardProps.title;
+    let location = eventCardProps.location;
+    let image = eventCardProps.image;
+
     return (
-        <></>
+        <Card>
+            <Card.Img variant="top" src={image} />
+            <Card.Body>
+            <Card.Title>{title}</Card.Title>
+            <Card.Text>
+                {date}
+            </Card.Text>
+            <Card.Text>
+                {location}
+            </Card.Text>            
+            </Card.Body>
+        </Card>
     )
 }
 
